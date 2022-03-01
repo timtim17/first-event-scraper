214baf056d8263f3ef0b2df0d861374cb476280b
Austin Jenchi <ajenchi9@gmail.com>
2022-02-28 20:04:07 -0800

    Write fresh FRC data to DynamoDB

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -21,6 +21,7 @@ declare global {
     }
 }
 
+// TODO: refactor
 interface Match {
     startTime: string
     teams: object[]
@@ -35,8 +36,11 @@ export const handler = async (
     const program = event.pathParameters!.program!.toUpperCase();
     const eventKey = event.pathParameters!.eventKey!;
 
+    console.log(`Handling request for ${program} ${eventKey}`);
+
     const isProgramValid = Object.keys(FIRSTPrograms).filter(p => program == p).length > 0;
     if (!isProgramValid) {
+        console.error('Invalid program');
         return {
             statusCode: 400,
             body: JSON.stringify({
@@ -47,6 +51,7 @@ export const handler = async (
 
     if (program == FIRSTPrograms.FRC) {
         if (!FRC_EVENT_KEY_REGEX.test(eventKey)) {
+            console.error('Invalid FRC event key');
             return {
                 statusCode: 400,
                 body: JSON.stringify({
@@ -56,6 +61,7 @@ export const handler = async (
         }
     }
 
+    console.log('Reading DynamoDB');
     const ddb = new DynamoDB({});
     const existingResults = await ddb.query({
         TableName: process.env.TABLE_NAME,
@@ -71,17 +77,20 @@ export const handler = async (
     });
 
     if (existingResults.Count && existingResults.Count > 0) {
+        console.log('Results found, returning');
         return {
             statusCode: 200,
             body: JSON.stringify(existingResults.Items),
         };
     } else {
+        console.log('No results found, fetching fresh data');
         // TODO: this should only be done once.
         //       invoke a lambda and repeat get until fulfilled?
         if (program == FIRSTPrograms.FRC) {
             const matches = eventKey.match(FRC_EVENT_KEY_REGEX)!;
             const year = matches[1];
             const eventId = matches[2];
+            console.log('Creating GET requests to FRC API');
             const promises = ['Qualification', 'Playoff'].map(level => 
                 axios.get(FRC_BASE_URL + `/${year}/schedule/${eventId}/${level}/hybrid`, {
                     headers: {
@@ -90,6 +99,7 @@ export const handler = async (
                     },
                 }));
             const [quals, elims] = (await axios.all(promises)).map(x => x.data);
+            console.log('GET requests fulfilled');
             const matchSchedule = quals.Schedule.concat(elims.Schedule);
             const matchObjects: Match[] = matchSchedule.map((match: any) => ({  // TODO: any bad
                 startTime: match.startTime,
@@ -100,13 +110,56 @@ export const handler = async (
                     match.tournamentLevel == 'Playoff' ?
                         (match.description.startsWith('Quarterfinal') ? 'qf'
                             : (match.description.startsWith('Semifinal') ? 'sf' : 'f'))
-                    : 'q') + match.description.match(/.+(\d+)/)[1],
+                    : 'q') + match.description.match(/(\d+)/g)[0],
             }));
+            console.log('Start write to DynamoDB...');
+            const writeRequests = matchObjects.map(match => {
+                let request = {
+                    PutRequest: {
+                        Item: {
+                            EventKey: {
+                                S: eventKey,
+                            },
+                            MatchId: {
+                                S: match.matchId
+                            },
+                            StartTime: {
+                                S: match.startTime
+                            },
+                            Teams: {
+                                L: match.teams.map(team => ({ S: JSON.stringify(team) })),
+                            },
+                        }
+                    }
+                };
+                if (match.scoreRedFinal != undefined && match.scoreBlueFinal != undefined) {
+                    // TODO: this is jank
+                    (request.PutRequest.Item as any)['ScoreRedFinal'] = {
+                        N: match.scoreRedFinal.toString()
+                    };
+                    (request.PutRequest.Item as any)['ScoreBlueFinal'] = {
+                        N: match.scoreBlueFinal.toString()
+                    };
+                }
+                return request;
+            });
+            let writePromises = [];
+            for (let i = 0; i < writeRequests.length; i += 25) {
+                writePromises.push(ddb.batchWriteItem({
+                    RequestItems: {
+                        [process.env.TABLE_NAME!]: writeRequests.slice(i, i + 25),
+                    }
+                }));
+            }
+            await Promise.all(writePromises)
+                .catch(err => console.error(`Failed to write to DDB [${eventKey}]: ${err}`));
+            console.log('Done!');
             return {
                 statusCode: 200,
                 body: JSON.stringify(matchObjects),
             }
         } else {
+            console.warn('Fetching fresh data not implemented yet.');
             return {
                 statusCode: 501,
                 body: JSON.stringify({
