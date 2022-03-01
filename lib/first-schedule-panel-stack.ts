1ebd525d3e3d3ee126f4f0c0ce2b0258356fe581
Austin Jenchi <ajenchi9@gmail.com>
2022-02-28 17:35:34 -0800

    Add basic CDK structure

--- lib/first-schedule-panel-stack.ts
+++ lib/first-schedule-panel-stack.ts
@@ -1,16 +1,52 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
+
+import { join } from 'path';
+
+import * as dotenv from 'dotenv';
+import { assert } from 'console';
+
+dotenv.config();
+const FRC_API_KEY = process.env.FRC_API_KEY as string;
+assert(FRC_API_KEY, 'Missing FRC API key.');
 
 export class FirstSchedulePanelStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const iamRole = new iam.Role(this, 'EventHistoryRole', {
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FirstSchedulePanelQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new dynamodb.Table(this, 'EventDB', {
+        partitionKey: { name: 'EventKey', type: dynamodb.AttributeType.STRING },
+        sortKey: { name: 'MatchNum', type: dynamodb.AttributeType.NUMBER },
+        billingMode: dynamodb.BillingMode.PROVISIONED,
+        readCapacity: 5,    // TODO: actually figure out proper numbers
+        writeCapacity: 2,
+    });
+    table.grantReadWriteData(iamRole);
+
+    const lambda = new NodejsFunction(this, 'GetEventHistory', {
+        entry: join(__dirname, '../src/lambda/index.ts'),
+        environment: {
+            FRC_API_KEY,
+        },
+        role: iamRole,
+        logRetention: logs.RetentionDays.ONE_WEEK,
+    })
+
+    const api = new apigw.LambdaRestApi(this, 'EventHistoryEndpoint', {
+        handler: lambda,
+        proxy: false,
+    });
+    const program = api.root.addResource('{program}');
+    const event = program.addResource('{eventKey}');
+    event.addMethod('GET');
   }
 }
